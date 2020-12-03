5873538ebfc9f237e55a868ef726460463830e78
direa-osman <direa_o@hotmail.com>
2020-12-03 15:28:19 +0100

    Update str_search.java

--- src/etsn20_lab4/str_search.java
+++ src/etsn20_lab4/str_search.java
@@ -6,9 +6,9 @@ public class str_search {
 	public static void main(String[] args) {
 		
 	    Scanner userInput = new Scanner(System.in);
-	    System.out.println("Enter textfile to search (example.txt): ");
+	    System.out.println("Enter textfile to search (example.txt):... ");
 	    String textfile = userInput.nextLine();
-	    System.out.println("Enter pattern to search for: ");
+	    System.out.println("Enter pattern to search for:... ");
 	    String pattern = userInput.nextLine();
 
 		Scanner sc = new Scanner(str_search.class.getResourceAsStream(textfile));
